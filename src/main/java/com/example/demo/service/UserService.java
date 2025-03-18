f882933f934ce8448686c3b92cacfcfbc5d626a7
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-03-18 21:49:38 +0530

    Update UserService.java

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,9 @@
 package com.example.demo.service;
 
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class UserService {
 }
